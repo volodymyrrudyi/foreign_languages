c0d88085e207779b8f56f2d7695fbc8b4798d9b1
Volodymyr Rudyj <vladimir.rudoy@gmail.com>
2011-12-09 00:27:28 +0200

    Added articles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,4 +8,22 @@ class HomeController < ApplicationController
   def teaching_methods
   end
 
+  def teachers
+  end
+
+  def methods
+  end
+
+  def select_locale
+    loc = params[:locale]
+
+    if !loc.nil?
+      I18n.locale = params[:locale]
+    else
+      I18n.locale = I18n.default_locale
+    end
+
+    redirect_to url_for(:controller => :home, :action => :index)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ ForeignLanguages::Application.routes.draw do
   get "home/index"
   get "home/history"
   get "home/teaching_methods"
+  get "home/methods"
+  get "home/teachers"
+  get "home/select_locale"
   root :to => 'home#index'
 end
